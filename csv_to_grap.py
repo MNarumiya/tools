fac6c311d4ae40ed08ebb31e71f67907c0db6d0c
Narumiya <buddasls54@gmail.com>
2018-07-30 19:33:18 +0900

    複数の値をとるようにした

--- csv_to_grap.py
+++ csv_to_grap.py
@@ -19,12 +19,15 @@ def csv_to_grap(csv_file_name,save_name_pdf):
     for df_name in df.columns:
         print(str(i)+":"+df_name)
         i += 1
-    side_label_nameber,depth_label_number = map(int,input("横軸 縦軸は?数字を入れる:").split())
+    label_number = list(map(int,input("横軸 縦軸は?数字を入れる:").split()))
+    side_label_number = label_number[0]
     df_col_name = df.columns # データの横軸ラベル(パラメーター名)を取得
+    df_sorted = df.sort_values(by=df_col_name[side_label_number], ascending=True) # データを昇順でソート
 
-    df = df.sort_values(by=df_col_name[side_label_nameber], ascending=True) # データを昇順でソート
-    plt.plot(df_sorted[df_col_name[side_label_nameber]],df_sorted[df_col_name[depth_label_number]],marker="o")
-    plt.xlabel(df_sorted[df_col_name[side_label_nameber]].name)
+    for depth_label_number in label_number[1:]:
+        plt.plot(df_sorted[df_col_name[side_label_number]],df_sorted[df_col_name[depth_label_number]],marker="o")
+
+    plt.xlabel(df_sorted[df_col_name[side_label_number]].name)
     plt.ylabel(df_sorted[df_col_name[depth_label_number]].name)
     plt.legend()
     plt.savefig(save_name_pdf) # pdfファイルとして保存
@@ -33,7 +36,7 @@ def csv_to_grap(csv_file_name,save_name_pdf):
 if __name__ == '__main__':
     args = sys.argv # コマンドライン引数を受け取る
     if (len(args) < 3): # csvファイル名とPDFファイル名がない場合受け付けない
-        print("csv_to_grap.py csvファイル名 横ラベル たてラベル 保存したいPDfファイル名 で呼び出してください")
+        print("csv_to_grap.py csvファイル名 保存したいPDfファイル名 で呼び出してください")
     else:
         csv_file_name = args[1]
         save_name_pdf = args[2]
